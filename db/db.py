91019c8e598101ae66db3430bf25bb06dec52f29
CosminIordache <cosminionutiordache@gmail.com>
2023-11-28 20:43:17 +0100

    gradio ia

---
+++ db/db.py
@@ -0,0 +1,41 @@
+import psycopg2 as db
+import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno
+load_dotenv()
+
+dbname = os.getenv('DB_NAME')
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+
+table_name = 'images'
+
+# Establece una conexión a la base de datos con las variables de entorno
+try:
+    connection = db.connect(dbname=dbname, user=user, password=password, host=host)
+    cursor = connection.cursor()
+
+    # Crear una tabla para almacenar imágenes
+    create_table_query = f'''
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            id SERIAL PRIMARY KEY,
+            image_name VARCHAR(255) NOT NULL,
+            image_data BYTEA NOT NULL
+        );
+    '''
+    cursor.execute(create_table_query)
+    connection.commit()
+    print(f'Table "{table_name}" created successfully.')
+    print("Connection established")
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+    # Cerrar conexión a la base de datos
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
+
---
+++ main.py
@@ -0,0 +1,73 @@
+import base64
+import io
+from PIL import Image
+import gradio as gr
+from db import db
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+dbname = os.getenv('DB_NAME')
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+
+
+def convert_binary_image_to_pil_image(binary_image_data):
+    image = Image.open(io.BytesIO(binary_image_data))
+    return image
+
+def upload_image(name, image_file):
+    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+    cur = conn.cursor()
+
+    with open(image_file, "rb") as f:
+        image_bytes = f.read()
+
+    cur.execute("""
+        INSERT INTO images (image_name, image_data) VALUES (%s, %s) RETURNING image_data;
+    """, (name, image_bytes))
+
+    # Recuperar la imagen recién insertada
+    inserted_image_data = cur.fetchone()[0]
+
+    conn.commit()
+    conn.close()
+
+    # Convertir la imagen a formato PIL antes de devolverla
+    inserted_image_pil = convert_binary_image_to_pil_image(inserted_image_data)
+
+    return inserted_image_pil
+
+
+
+def get_uploaded_images():
+    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT image_name, image_data FROM images;
+    """)
+    images = []
+    for image_name, image_data in cur:
+        image_conv = convert_binary_image_to_pil_image(image_data)
+        # Add image to list of images
+        images.append((image_name, image_conv))
+
+    conn.close()
+    return images
+
+
+
+interface = gr.Interface(
+    fn=upload_image,
+    inputs=[
+        gr.Textbox(label="Name"),
+        gr.File(label="Image"),
+    ],
+    outputs=[
+        gr.Image()
+    ],
+    title="Upload Image to PostgreSQL"
+)
+interface.launch(share=True)
